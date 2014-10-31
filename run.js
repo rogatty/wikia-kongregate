8060637bcb2e1196d49842c67ac8d0d137e8d246
Igor Rogatty <igor@wikia-inc.com>
2014-10-31 15:45:54 +0100

    Create article string from data

--- run.js
+++ run.js
@@ -1,6 +1,8 @@
 var fs = require('fs'),
 	xml2js = require('xml2js'),
 	request = require('request'),
+	HtmlEntities = require('html-entities').Html5Entities,
+	htmlEntities,
 	parser = new xml2js.Parser(),
 	counter = 0;
 
@@ -12,22 +14,22 @@ function scrape(url) {
 
 function parseData(data) {
 	parser.parseString(data, function (err, result) {
-		result.gameset.game.forEach(parseGameData);
+		result.gameset.game.slice(0,60).forEach(parseGameData);
 	});
 }
 
-function parseGameData(game) {
-	if (parseFloat(game.rating) >= 4) {
-		//console.log('Checking if ' + game.title + ' exists on Kongregate Wikia');
-		request('http://kongregate.wikia.com/' + game.title, function (error, response) {
+function parseGameData(data) {
+	if (parseFloat(data.rating) >= 4) {
+		//console.log('Checking if ' + data.title + ' exists on Kongregate Wikia');
+		request('http://kongregate.wikia.com/' + data.title, function (error, response) {
 			if (!error && response.statusCode == 404) {
-				organizeGameData(game);
+				organizeGameData(data);
 			}
 		});
 	}
 }
 
-function organizeGameData(game) {
+function organizeGameData(data) {
 	var organizedData = {
 		title: null,
 		launchDate: null,
@@ -39,20 +41,49 @@ function organizeGameData(game) {
 		developerName: null
 	};
 
-	organizedData.title = game.title[0];
-	organizedData.launchDate = game.launch_date[0];
-	organizedData.category = game.category[0];
+	organizedData.title = data.title[0];
+	organizedData.launchDate = data.launch_date[0];
+	organizedData.category = data.category[0];
 
-	if (game.screenshot) {
-		organizedData.screenshot = game.screenshot[0];
+	if (data.screenshot) {
+		organizedData.screenshot = data.screenshot[0];
 	}
 
-	organizedData.url = game.url[0];
-	organizedData.description = game.description[0];
-	organizedData.instructions = game.instructions[0];
-	organizedData.developerName = game.developer_name[0];
+	organizedData.url = data.url[0];
+	organizedData.description = data.description[0];
+	organizedData.instructions = data.instructions[0];
+	organizedData.developerName = data.developer_name[0];
 
-	console.log('###', organizedData);
+	//console.log(organizedData);
+	createArticle(organizedData);
 }
 
+function createArticle(data) {
+	var articleBuffer = [
+		'{{Game',
+		'|name =[' + data.url + ' ' + data.title + ']',
+		'|image =' + data.screenshot,
+		'|author =[http://www.kongregate.com/accounts/' + data.developerName + ' ' + data.developerName + ']',
+		'|genre =' + data.category,
+		'|pub =' + data.launchDate,
+		'|iga = ',
+		'|type = ',
+		'|upg = ',
+		'}}'
+	].join('\n');
+
+	articleBuffer += '\n' + fixNewLines(data.description);
+
+	if (data.instructions) {
+		articleBuffer += '\n==Controls==\n' + fixNewLines(data.instructions);
+	}
+
+	console.log(articleBuffer);
+}
+
+function fixNewLines(str) {
+	return htmlEntities.decode(str).split('\n').join('<br>\n');
+}
+
+htmlEntities = new HtmlEntities();
 scrape('http://www.kongregate.com/games_for_your_site.xml');
